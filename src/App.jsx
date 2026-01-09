561a27f0482d9e19276d4e933c6ace0805bc5ccd
Vlada Labza <vlada.labza@gmail.com>
2026-01-09 19:04:07 +0200

    jjj

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { Routes, Route, useLocation } from "react-router-dom";
+import MenuPage from "./MenuPage/MenuPage";
+import RampsProductDetail from "./RampsProductDetail/RampsProductDetail";
+import SkateparksProductDetail from "./SkateparksProductDetail/SkateparksProductDetail";
+import DiyProductDetail from "./SetsProductDetail/SetsProductDetail";
+import ProjectPage from "./ProjectPage/ProjectPage"
 import './App.css'
+import SetsProductDetail from "./SetsProductDetail/SetsProductDetail";
+import Catalogue from "./Catalogue/Catalogue";
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ const location = useLocation();
+ 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
+    <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<MenuPage />} />
+        
+        <Route  path="/catalogue" element={<Catalogue />} />
+    
+       <Route path="/product/sets/:id" element={<SetsProductDetail />} />
+ <Route path="/product/ramps/:id" element={<RampsProductDetail />} /> 
+ <Route path="/product/skateparks/:id" element={<SkateparksProductDetail />} />
+ <Route path="/product/diy/:id" element={<DiyProductDetail />} />
+       
+
+
+    
+        
+         <Route path="projectpage" element={<ProjectPage/>}/>
+        
+      </Routes>
+     
     </>
   )
 }
