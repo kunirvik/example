60cd13e110784e1e90140234b875cea6b15ae0c0
Vlada Labza <vlada.labza@gmail.com>
2026-03-10 21:20:45 +0200

    5

--- src/SetsProductDetail/SetsProductDetail.jsx
+++ src/SetsProductDetail/SetsProductDetail.jsx
@@ -796,7 +796,6 @@ export default function SetsProductDetail() {
                   alt={currentProduct.name}
                   className="object-contain"
                   style={{
-                    scale: 2,
                     position: "absolute",
                     top: 0,
                     left: 0,
